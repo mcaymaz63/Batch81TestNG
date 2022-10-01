e047666ba62eb14572632cb4a19ddaaaa7a17614
mevlut caymaz <mevlitkacmaz.63@gmail.com>
2022-10-01 22:18:11 +0300

    TestNG

---
+++ src/test/java/tests/day19/C01_SingletonPattern.java
@@ -0,0 +1,28 @@
+package tests.day19;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HmcPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C01_SingletonPattern {
+
+    @Test
+    public void test01() {
+        //Driver obj = new Driver();
+        //obj.getDriver().get(ConfigReader.getProperty("amznUrl"));
+        /*
+        SingletonPattern : tekli kullanım demektir. Bir class'ın farklı class'lardan
+        obje oluşturarak kullanımını engellemektir.
+         */
+
+
+    }
+
+
+        }
+
+
